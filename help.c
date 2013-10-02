d630f7883a344162da240770c10ea5dff91ce990
matthewlee0725 <matthewlee0725@gmail.com>
2013-10-02 12:22:55 +0800

    Create help.c

---
+++ help.c
@@ -0,0 +1,10 @@
+#include "bingw.h"
+
+void help()
+{
+	printf("NOTICE: IMAGE WILL BE SAVED AT \"$~/Bing/\"\n");
+	printf("bingw [OPTION]\n");
+	printf("-c: Use bing latest image as background\n");
+	printf("-d: Just download bing latest image\n");
+	printf("-h: Help\n");
+}
